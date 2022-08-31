d712d9f2e665e45b42c8d1f05c195e1a241bba63
lengyellukas <110744464+lengyellukas@users.noreply.github.com>
2022-08-31 23:53:39 +0200

    commands used in the project added

--- commands.sh
+++ commands.sh
@@ -1,6 +1,11 @@
 git clone git@github.com:lengyellukas/devops_nano_pipeline.git
 cd devops_nano_pipeline
+make install
 git pull
 make all
-az webapp up -n devops_nano_pipeline
+az webapp up -n devops-nano-pipeline
+git add .
+git commit -m "commit message"
+git push
 ./make_predict_azure_app.sh
+locust
